38e33845573e750de967c34326a212cb5f544160
Connoye <gordon.raeshawn@gmail.com>
2022-08-30 09:15:22 -0500

    Save

---
+++ FirstProgram/demo.js
@@ -0,0 +1,7 @@
+// var http = required("http");
+// http.createServer(function (req, res) {
+//     res.write("Hello world! Welcome to this tutorial");
+//     res.end();
+// }).listen(8187);
+var msg = 'Hello World';
+console.log(msg);
